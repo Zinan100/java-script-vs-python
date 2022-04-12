bcc113791e37c332285fb9109f2aafd03b5cbf60
KC Khais <52489385+kckhais@users.noreply.github.com>
2022-04-12 22:46:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -248,11 +248,7 @@ async def start(client, message):
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
                 try:
-                    f_caption=CUSTOM_FILE_CAPTION.format(mention=message.from_user.mention,
-                                                         file_name='' if title is None else title,
-                                                         file_size='' if size is None else size,
-                                                         file_caption='' if f_caption is None else f_caption,
-                                                         title=message.chat.title)
+                    f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='')
                 except:
                     return 
             await msg.edit_caption(f_caption)
@@ -266,11 +262,7 @@ async def start(client, message):
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
         try:
-            f_caption=CUSTOM_FILE_CAPTION.format(mention=message.from_user.mention,
-                                                file_name='' if title is None else title,
-                                                file_size='' if size is None else size,
-                                                file_caption='' if f_caption is None else f_caption,
-                                                title=message.chat.title)
+            f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
         except Exception as e:
             logger.exception(e)
             f_caption=f_caption
